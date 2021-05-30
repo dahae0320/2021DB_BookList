17b14c4df371ba1320bda70cbefd7ab2ba81feff
dahae0320 <ldh990320ldh@gmail.com>
2021-05-30 21:49:54 +0900

    update

--- DBProject/bookread.php
+++ DBProject/bookread.php
@@ -2,7 +2,7 @@
     require("functions.php");
     
     $conn = connectDB(); // DB 연결 객체 획득
-    $books = selectAlreadyReadPageBook($conn); // DB에서 메인페이지 책 가지고 오기
+    $books = selectAlreadyReadPageBook($conn); // DB에서 내가 읽은 책 페이지 책 가지고 오기
     $user_list = selectUser($conn); // DB에서 USER 드랍다운에 들어갈 USER 데이터 가지고 오기
 
     view('bookread',$books,$user_list); // 레이아웃 호출
